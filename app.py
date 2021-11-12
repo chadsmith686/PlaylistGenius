a19e06f932164410f85aa0f210610dc5504cac51
Chad Smith <chadsmithmusic@icloud.com>
2021-11-11 16:17:40 -0800

    added base and index templates

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
 from flask import Flask, redirect, render_template
 from flask_debugtoolbar import DebugToolbarExtension
 from models import connect_db
+from forms import SearchForm
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///playlist-genius'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = "its a secret!"
 
 connect_db(app)
-db.create_all()
+# db.create_all()
 
-app.config['SECRET_KEY'] = "its a secret!"
+debug = DebugToolbarExtension(app)
+
+@app.route('/')
+def root():
+    """Displays home page."""
+    return render_template('index.html')
+
