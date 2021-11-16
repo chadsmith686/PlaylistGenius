e61aa4f937f216599bb115b8c44d490ff547f38e
Chad Smith <chadsmithmusic@icloud.com>
2021-11-16 04:52:18 -0800

    added API route

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask, redirect, render_template
+import requests
+from flask import Flask, redirect, render_template, request
 from flask_debugtoolbar import DebugToolbarExtension
 from models import connect_db
-from forms import SearchForm
+from secret import USER_AGENT, API_SECRET_KEY
+
+API_BASE_URL = "https://ws.audioscrobbler.com/2.0/"
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///playlist-genius'
@@ -19,3 +22,21 @@ def root():
     """Displays home page."""
     return render_template('index.html')
 
+
+def lastfm_get(payload):
+    # define headers and URL
+    headers = {'user-agent': USER_AGENT}
+    url = 'https://ws.audioscrobbler.com/2.0/'
+
+    # Add API key and format to the payload
+    payload['api_key'] = API_SECRET_KEY
+    payload['format'] = 'json'
+
+    response = requests.get(url, headers=headers, params=payload)
+    return response
+
+@app.route('/go')
+def get_similar_tracks():
+    artist = request.args["artist"]
+    track = request.args["track"]
+    requests.get(f"{API_BASE_URL}/", params={'artist': artist, 'track': track})
