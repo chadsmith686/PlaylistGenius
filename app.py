3523618bf4b0b82d3d1833982ab5e18defe1ffbc
Chad Smith <chadsmithmusic@icloud.com>
2021-11-29 19:03:46 -0800

    inserted loop to add tracks to db

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import requests
 from flask import Flask, render_template, request
 from flask_debugtoolbar import DebugToolbarExtension
 from flask_sqlalchemy import SQLAlchemy
-from models import connect_db
+from models import db, connect_db, Song
 from secret import API_KEY 
 
 API_BASE_URL = "https://ws.audioscrobbler.com/2.0/"
@@ -13,8 +13,8 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SECRET_KEY'] = "its a secret!"
 
-db = SQLAlchemy(app)
 connect_db(app)
+db.create_all()
 
 debug = DebugToolbarExtension(app)
 
@@ -31,7 +31,15 @@ def get_similar_tracks():
     r = requests.get(f"{API_BASE_URL}?method=track.getsimilar&artist={artist}&track={track}&api_key={API_KEY}&format=json", 
                     params={'artist': artist, 'track': track}, headers=headers)
 
-    data = r.json()
+    data = r.json() # convert json data into string
     tracks = data["similartracks"]["track"]
+
+    # loop over tracks and add to db
+    for track in tracks:
+        artist_name = track["artist"]["name"]
+        song_name = track["name"]
+        insert_song = Song(song_name=song_name, artist_name=artist_name)
+        db.session.add(insert_song)
+    db.session.commit()
 
     return render_template("results.html", data=data, tracks=tracks, artist=artist, track=track)
