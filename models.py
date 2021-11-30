e8b1e30db608058558006307cf05332b96a22ace
Chad Smith <chadsmithmusic@icloud.com>
2021-11-29 19:03:04 -0800

    fixed foreign keys

--- models.py
+++ models.py
@@ -3,28 +3,32 @@ from sqlalchemy.orm import backref, relationship
 
 db = SQLAlchemy()
 
+
+class Playlist(db.Model):
+
+    __tablename__ = 'playlist'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.Text, nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    songs = db.relationship('Song', secondary='playlists_songs', backref='playlist')
+
 class Song(db.Model):
 
-    __tablename__ = '__songs__'
+    __tablename__ = 'song'
 
     id = db.Column(db.Integer, primary_key=True)
     song_name = db.Column(db.String, nullable=False)
-    songs = db.relationship('Artist', secondary='songs_artists', backref='Song')
-
-class Artist(db.Model):
-
-    __tablename__ = '__artists__'
-
-    id = db.Column(db.Integer, primary_key=True)
     artist_name = db.Column(db.String, nullable=False)
 
-class SongArtist(db.Model):
+class PlaylistSong(db.Model):
+    """Mapping of a playlist to a song."""
 
-    __tablename__ = '__songs_artists__'
+    _tablename_ = "playlists_songs"
 
-    id = db.Column(db.Integer, primary_key=True)
-    song_id = db.Column(db.Integer, db.ForeignKey("songs.id"))
-    artist_id = db.Column(db.Integer, db.ForeignKey("playlists.id"))
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    song_id = db.Column(db.Integer, db.ForeignKey("song.id"))
+    playlist_id = db.Column(db.Integer, db.ForeignKey("playlist.id"))
 
 def connect_db(app):
     """Connect to database."""
