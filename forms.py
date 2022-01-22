75eb392e19e0bffa0bdb9e64e78537ad5d2b4554
Chad Smith <chadsmithmusic@icloud.com>
2022-01-22 14:08:40 -0800

    heroku merge

--- forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import InputRequired
-
-class SearchForm(FlaskForm):
-    """Form for searching."""
-
-    title = StringField("Song Title", validators=[InputRequired()])
-    artist = StringField("Artist Name", validators=[InputRequired()])
