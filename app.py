f60a94bc496a56a916c92cd3bc102468bb2942e5
Chad Smith <chadsmithmusic@icloud.com>
2022-01-25 18:13:47 -0800

    update sqlalchemy database_uri syntax

--- app.py
+++ app.py
@@ -6,8 +6,10 @@ from secret import API_KEY
 API_BASE_URL = "https://ws.audioscrobbler.com/2.0/"
 
 app = Flask(__name__,  static_url_path='')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
-    'DATABASE_URL', 'postgresql:///playlist-genius')
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+    'DATABASE_URL', "postgres:///picslink").replace("://", "ql://", 1)
+app.config["SECRET_KEY"] = os.environ.get('SECRET_KEY', 'hellosecret1')
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'hellosecret1')
